2c808534c6d49a10d29d9224bec5331bb228bf34
Gino Fiori <gino.fiori@gmail.com>
2023-08-09 07:33:31 -0300

    modificaciones en navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import CTA from './CTA'
 import ME from '../../assets/me-5.png'
 import HeaderSocials from './HeaderSocials'
 import { LanguageContext } from '../../context/LanguageContext';
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
     const { language } = useContext(LanguageContext);
@@ -13,8 +14,8 @@ const Header = () => {
                 <h5 className={`language-transition ${language === 'en' ? 'language-visible' : ''}`}>Hello I'm</h5>
                 <h5 className={`language-transition ${language === 'es' ? 'language-visible' : ''}`}>¡Hola! Soy</h5>
                 <h1>Gino Fiori</h1>
-                <h5 className={`text-light language-transition ${language === 'en' ? 'language-visible' : ''}`}>Software Developer</h5>
-                <h5 className={`text-light language-transition ${language === 'es' ? 'language-visible' : ''}`}>Desarrollador de Software</h5>
+                <h5 className={`text-light monoscope language-transition ${language === 'en' ? 'language-visible' : ''}`}><Typewriter options={{strings:['Software Developer'],autoStart:true,loop:true}} /></h5>
+                <h5 className={`text-light monoscope language-transition ${language === 'es' ? 'language-visible' : ''}`}><Typewriter options={{strings:['Desarrolador de Software'],autoStart:true,loop:true}}/></h5>
                 <CTA/>
                 <HeaderSocials/>
                 <div className="me">
