a5b34a20dc0c4d076ad47f5379fbd62ff5c6ba72
Gino Fiori <gino.fiori@gmail.com>
2023-09-23 19:02:48 -0300

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import {LanguageProvider} from './context/LanguageContext'
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
     return (
@@ -18,6 +19,7 @@ const App = () => {
             <Portfolio/>
             <Contact/>
             <Footer/>
+            <Analytics />
         </LanguageProvider>
     )
 }
