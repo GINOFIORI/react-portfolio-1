89518df8ba1b56299f4772b9629012d51512f721
Gino Fiori <gino.fiori@gmail.com>
2023-10-08 17:36:59 -0300

    Ultimo commit para publicacion

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,18 +16,22 @@ const Portfolio = () => {
 
             <div className="container portfolio__container">
                 <article className="portfolio__item">
+                    <span className={`portfolio__notification language-transition ${language === 'en' ? 'language-visible' : ''}`}>Available soon!</span>
+                    <span className={`portfolio__notification language-transition ${language === 'es' ? 'language-visible' : ''}`}>Proximamente!</span>
                     <div className="portfolio__item-image">
                         <img src={IMG1} alt=""/>
                     </div>
                     <h3>IT CRM</h3>
                     <h5>Software de Gestión de Proyectos</h5>
                     <div className="portfolio__item-cta">
-                        <a href="https://crm.neosistemassrl.com" className="btn btn-primary" target="_blank">Demo</a>
+                        <a href="https://crm.neosistemassrl.com" className="btn btn-primary btn-disabled" target="_blank">Demo</a>
                     </div>
                     <p className="small">User: demo@demo.com</p>
                     <p className="small">Pass: demo</p>
                 </article>
                 <article className="portfolio__item">
+                    <span className={`portfolio__notification language-transition ${language === 'en' ? 'language-visible' : ''}`}>Available soon!</span>
+                    <span className={`portfolio__notification language-transition ${language === 'es' ? 'language-visible' : ''}`}>Proximamente!</span>
                     <div className="portfolio__item-image">
                         <img src={IMG3} alt=""/>
                     </div>
@@ -39,6 +43,8 @@ const Portfolio = () => {
                     </div>
                 </article>
                 <article className="portfolio__item">
+                    <span className={`portfolio__notification language-transition ${language === 'en' ? 'language-visible' : ''}`}>Available soon!</span>
+                    <span className={`portfolio__notification language-transition ${language === 'es' ? 'language-visible' : ''}`}>Proximamente!</span>
                     <div className="portfolio__item-image">
                         <img src={IMG2} alt=""/>
                     </div>
